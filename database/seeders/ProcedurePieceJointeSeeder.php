b9441ad3dba0386d37a65467efb0bc7db9f0e9d4
abdoulgango <abdoulahmedgango03@gmail.com>
2023-12-21 14:33:49 +0000

    21/12/23 1433

--- database/seeders/ProcedurePieceJointeSeeder.php
+++ database/seeders/ProcedurePieceJointeSeeder.php
@@ -15,7 +15,7 @@ class ProcedurePieceJointeSeeder extends Seeder
      */
     public function run(): void
     {
-        $procedureP001 = Procedure::where('code', 'P001')->first();
+        $procedureP002 = Procedure::where('code', 'P002')->first();
         $pj1 = PieceJointe::where('libelle', "Statut société")->first()->uuid;
         $pj2 = PieceJointe::where('libelle', "RCCM/RSCPM")->first()->uuid;
         $pj3 = PieceJointe::where('libelle', "IFU/Recepissé")->first()->uuid;
@@ -27,16 +27,28 @@ class ProcedurePieceJointeSeeder extends Seeder
         $pj9 = PieceJointe::where('libelle', "Déclaration sur l'honneur")->first()->uuid;
         $pj10 = PieceJointe::where('libelle', "CV")->first()->uuid;
         
-        $procedureP001->pieceJointe()->attach($pj1);
-        $procedureP001->pieceJointe()->attach($pj2);
-        $procedureP001->pieceJointe()->attach($pj3);
-        $procedureP001->pieceJointe()->attach($pj4);
-        $procedureP001->pieceJointe()->attach($pj5);
-        $procedureP001->pieceJointe()->attach($pj6);
-        $procedureP001->pieceJointe()->attach($pj7);
-        $procedureP001->pieceJointe()->attach($pj8);
-        $procedureP001->pieceJointe()->attach($pj9);
-        $procedureP001->pieceJointe()->attach($pj10);
+        $procedureP002->pieceJointe()->attach($pj1);
+        $procedureP002->pieceJointe()->attach($pj2);
+        $procedureP002->pieceJointe()->attach($pj3);
+        $procedureP002->pieceJointe()->attach($pj4);
+        $procedureP002->pieceJointe()->attach($pj5);
+        $procedureP002->pieceJointe()->attach($pj6);
+        $procedureP002->pieceJointe()->attach($pj7);
+        $procedureP002->pieceJointe()->attach($pj8);
+        $procedureP002->pieceJointe()->attach($pj9);
+        $procedureP002->pieceJointe()->attach($pj10);
+        $procedureP002->save();
+
+        $procedureP001 = Procedure::where('code', 'P001')->first();
+        $pj11 = PieceJointe::where('libelle', "PUH/Attestation d'attribution")->first()->uuid;
+        $pj12 = PieceJointe::where('libelle', "Plan de masse/plan d'implantation")->first()->uuid;
+        $pj13 = PieceJointe::where('libelle', "Vue en plan et coupe du batiment")->first()->uuid;
+        $pj14 = PieceJointe::where('libelle', "Copie CNIB")->first()->uuid;
+
+        $procedureP001->pieceJointe()->attach($pj11);
+        $procedureP001->pieceJointe()->attach($pj12);
+        $procedureP001->pieceJointe()->attach($pj13);
+        $procedureP001->pieceJointe()->attach($pj14);
         $procedureP001->save();
     }
 }
--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -2,7 +2,7 @@
         <div class="container d-flex justify-content-center justify-content-md-between">
             <div class="contact-info d-flex align-items-center"></div>
             <div class="contact-info d-flex align-items-center">
-                <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">procedure@environnement.gov.bf</a></i>
+                <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">procedure@mid.gov.bf</a></i>
                 <i class="bi bi-phone d-flex align-items-center ms-4"><span>+226 25307039</span></i>
             </div>
 
